cbd01ecfa342aca06b9fcdde042825ede8045cbb
sajrus33 <brianwala22@gmail.com>
2019-08-16 21:04:12 +0200

    tiny fix, destructing objects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./components/css/reset.css";
 import Board from "./components/Board";
 import List from "./components/List";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
+      draggedId: null,
       lists: [
         {
           txt: "List 1",
@@ -63,40 +64,42 @@ class App extends Component {
               id: "i8"
             }
           ]
-        },
-      ],
-    }
-    this.draggedId = null;
-
+        }
+      ]
+    };
   }
 
   dragStart = e => {
-    this.draggedId = e.target.id
-  }
+    this.setState({
+      draggedId: e.target.id
+    });
+  };
 
   dragOver = e => {
     e.preventDefault();
-  }
+  };
 
   drop = e => {
     const dropId = e.target.id;
-    const dragId = this.draggedId;
+    const dragId = this.state.draggedId;
 
     if (dropId !== dragId) {
-      const [dragPrefixId,] = dragId;
-      const [dropPrefixId,] = dropId;
+      const [dragPrefixId] = dragId;
+      const [dropPrefixId] = dropId;
 
       if (dragPrefixId === dropPrefixId) {
         const lists = [...this.state.lists];
 
         if (dragPrefixId === "l") {
-          const indexes = lists.filter(list => list.id === dragId || list.id === dropId);
-          const i1 = lists.indexOf(indexes[0]);
-          const i2 = lists.indexOf(indexes[1]);
-          lists.splice(i2, 1, lists.splice(i1, 1, lists[i2])[0]);
+          let [i1, i2] = lists.filter(
+            list => list.id === dragId || list.id === dropId
+          );
+          i1 = lists.indexOf(i1);
+          i2 = lists.indexOf(i2);
 
+          lists.splice(i2, 1, lists.splice(i1, 1, lists[i2])[0]);
         } else if (dragPrefixId === "i") {
-          const items = lists
+          const [item1, item2] = lists
             .map(list => list.items)
             .flat()
             .filter(item => item.id === dragId || item.id === dropId);
@@ -104,38 +107,41 @@ class App extends Component {
           const indexes = [];
           lists.forEach((list, iL) =>
             list.items.forEach((item, iI) => {
-              if (item.id === items[0].id || item.id === items[1].id) {
+              if (item.id === item1.id || item.id === item2.id) {
                 indexes.push([iL, iI]);
               }
-            }))
+            })
+          );
           const [[iL1, iI1], [iL2, iI2]] = indexes;
 
-          lists[iL1].items.splice(iI1, 1, lists[iL2].items.splice(iI2, 1, lists[iL1].items[iI1])[0]);
+          lists[iL1].items.splice(
+            iI1,
+            1,
+            lists[iL2].items.splice(iI2, 1, lists[iL1].items[iI1])[0]
+          );
         }
 
         this.setState({
           lists: lists
-        })
+        });
       }
-
     }
-  }
+  };
   render() {
-    let lists = this.state.lists.map((list, i) =>
-      (<List
+    let lists = this.state.lists.map((list, i) => (
+      <List
         items={list.items}
         listDragStart={this.dragStart.bind(this)}
         listDragOver={this.dragOver.bind(this)}
         droppable={list.droppable}
         listDrop={this.drop.bind(this)}
-        txt={list.txt} key={i} id={list.id} >
-      </List>))
+        txt={list.txt}
+        key={i}
+        id={list.id}
+      />
+    ));
 
-    return (
-      <Board>
-        {lists}
-      </Board>
-    )
+    return <Board>{lists}</Board>;
   }
 }
 
